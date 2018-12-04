8cbe07ee4eb1237250c947b0c8d102ee7e30b7a2
speug <lauri.seppalainen@aalto.fi>
2018-12-04 16:10:44 +0200

    added tests, found that last letter would not print, rewrote

--- 2day/ass2.py
+++ 2day/ass2.py
@@ -1,18 +1,25 @@
 with open('input.txt') as fp:
     lines = fp.readlines()
     s = len(lines)
-    lenOfWord = len(lines[0]) - 2
+    lenOfWord = len(lines[0]) - 1
     common = ''
     for i in range(0, s):
-        curr = lines(i)
-            for j in range(i+1,s):
-                currComp = lines(j)
-                stopFlag = False
-                for k in range(0, lenOfWord):
-                    if curr[k] == currComp[k]:
-                        common += curr[k]
+        curr = lines[i]
+        for j in range(i+1,s):
+            currComp = lines[j]
+#            print('Comparing {0} and {1}'.format(curr, currComp))
+            stopFlag = False
+            for k in range(0, lenOfWord):
+                if curr[k] == currComp[k]:
+                    common += curr[k]
+                else:
+                    if stopFlag:
+                        common = ''
+                        break
                     else:
-                        if !stopFlag:
-                            break
+                        stopFlag = True
+            if common != '':
                 break
+        if common != '':
+            break
     print('Common letters: ' + common)
